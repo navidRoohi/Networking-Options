f8f72040085be368bafdc835e2db016302b5ae4d
Navid Roohi <navidRoohi@users.noreply.github.com>
2016-09-23 08:24:08 -0700

    Update SimpleHTTP.java

--- SimpleHTTP.java
+++ SimpleHTTP.java
@@ -11,7 +11,6 @@ import java.io.IOException;
  
     public static void main(String[] args){
 
-           // Another example for networking
         String sConvertedResponse = "";
         JSONObject jsonObjectMain = null;
         JSONArray jsonArray = null;
